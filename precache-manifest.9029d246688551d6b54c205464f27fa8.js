03da27f231d68b52dba9081a8ec5b346325132ba
johaol16 <johanna.n.olsson@hotmail.com>
2021-02-01 09:31:44 +0100

    Add files

---
+++ precache-manifest.9029d246688551d6b54c205464f27fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa398cbfa23997928778",
+    "url": "/css/app.584fd98b.css"
+  },
+  {
+    "revision": "b34c75820ac87d5d29def7b4fe42c50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa398cbfa23997928778",
+    "url": "/js/app.cf2b6844.js"
+  },
+  {
+    "revision": "563a681f473b65a6bc59",
+    "url": "/js/chunk-vendors.215e6f2c.js"
+  },
+  {
+    "revision": "68a359529502a64beaa4d988a67b8d96",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
